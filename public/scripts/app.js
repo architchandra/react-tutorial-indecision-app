6b5b15628b55cf14cc7d1c52a377ca1157a376c7
Archit Chandra <architchandra@gmail.com>
2021-06-16 11:09:04 +0530

    Change directory structure, set up Babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,10 @@
-var template = <p>This is app.</p>;
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'This is app.'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
