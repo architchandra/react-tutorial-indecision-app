7167d75034161b004fc02c7513cee1005d33295f
Archit Chandra <architchandra@gmail.com>
2021-07-10 22:36:27 +0530

    Add subtitle and options props to app data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,28 @@
 'use strict';
 
+var appData = {
+  title: 'Indecision App',
+  subtitle: 'Do things with clarity',
+  options: ['One', 'Two']
+};
+
 var template = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    'This is zappsy.'
+    appData.title
+  ),
+  appData.subtitle && React.createElement(
+    'p',
+    null,
+    appData.subtitle
   ),
   React.createElement(
     'p',
     null,
-    'This is some text.'
+    appData.options && appData.options.length ? 'Options available: ' + appData.options.join(', ') : 'No options found.'
   )
 );
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
+var appData = {
+  title: 'Indecision App',
+  subtitle: 'Do things with clarity',
+  options: ['One', 'Two'],
+};
+
 var template = (
   <div>
-    <h1>This is zappsy.</h1>
-    <p>This is some text.</p>
+    <h1>{ appData.title }</h1>
+    { appData.subtitle && <p>{appData.subtitle}</p> }
+    <p>{ appData.options && appData.options.length ? 'Options available: ' + appData.options.join(', ') : 'No options found.' }</p>
   </div>
 );
 var appRoot = document.getElementById('app');
