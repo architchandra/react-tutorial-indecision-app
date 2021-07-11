6d35443e870db9ee3f6c8f84c2058b6762c513f9
Archit Chandra <architchandra@gmail.com>
2021-07-11 19:58:02 +0530

    Create counter template

--- src/app.js
+++ src/app.js
@@ -13,4 +13,31 @@ const template = (
 );
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+let count = 0;
+
+const plusOne = () => {
+  count++;
+  reRender();
+};
+const minusOne = () => {
+  count--;
+  reRender();
+};
+const reset = () => {
+  count = 0;
+  reRender();
+};
+
+const reRender = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: { count }</h1>
+      <button onClick={ () => plusOne() }>+1</button>
+      <button onClick={ () => minusOne() }>-1</button>
+      <button onClick={ () => reset() }>Reset</button>
+    </div>
+  );
+  
+  ReactDOM.render(templateTwo, appRoot);
+};
+reRender();
