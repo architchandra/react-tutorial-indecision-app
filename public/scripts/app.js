9328bbeb054ceb68cbd5482cd5274c7c4d711e69
Archit Chandra <architchandra@gmail.com>
2021-07-11 23:19:30 +0530

    Enable adding and removing options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,28 +3,60 @@
 var appData = {
   title: 'Indecision App',
   subtitle: 'Do things with clarity',
-  options: ['One', 'Two']
+  options: []
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    appData.title
-  ),
-  appData.subtitle && React.createElement(
-    'p',
-    null,
-    appData.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    appData.options && appData.options.length ? 'Options available: ' + appData.options.join(', ') : 'No options found.'
-  )
-);
+var onClickRemove = function onClickRemove(e) {
+  e.preventDefault();
+  appData.options = [];
+  reRender();
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+  appData.options.push(option);
+  e.target.elements.option.value = '';
+  reRender();
+};
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var reRender = function reRender() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      appData.title
+    ),
+    appData.subtitle && React.createElement(
+      'p',
+      null,
+      appData.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      appData.options && appData.options.length ? 'Options available: ' + appData.options.join(', ') : 'No options found.'
+    ),
+    React.createElement(
+      'button',
+      { onClick: onClickRemove },
+      'Remove All'
+    ),
+    React.createElement(
+      'p',
+      null,
+      appData.options.length
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' })
+    )
+  );
+  ReactDOM.render(template, appRoot);
+};
+reRender();
--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
 const appData = {
   title: 'Indecision App',
   subtitle: 'Do things with clarity',
-  options: ['One', 'Two'],
+  options: [],
 };
 
-const template = (
-  <div>
-    <h1>{ appData.title }</h1>
-    { appData.subtitle && <p>{appData.subtitle}</p> }
-    <p>{ appData.options && appData.options.length ? 'Options available: ' + appData.options.join(', ') : 'No options found.' }</p>
-  </div>
-);
+const onClickRemove = (e) => {
+  e.preventDefault();
+  appData.options = [];
+  reRender();
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  appData.options.push(option);
+  e.target.elements.option.value = '';
+  reRender();
+};
+
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+const reRender = () => {
+  const template = (
+    <div>
+      <h1>{ appData.title }</h1>
+      { appData.subtitle && <p>{appData.subtitle}</p> }
+      <p>
+        { appData.options && appData.options.length ? 'Options available: ' + appData.options.join(', ') : 'No options found.' }
+      </p>
+      <button onClick={ onClickRemove }>Remove All</button>
+      <p>{ appData.options.length }</p>
+      <form onSubmit={ onFormSubmit }>
+        <input type="text" name="option" />
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+};
+reRender();
