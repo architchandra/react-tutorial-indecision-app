febdcc5608569fbac3006321c7e4c0761320154a
Archit Chandra <architchandra@gmail.com>
2021-07-11 15:29:46 +0530

    Use ES6 variable declaration methods

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-var appData = {
+const appData = {
   title: 'Indecision App',
   subtitle: 'Do things with clarity',
   options: ['One', 'Two'],
 };
 
-var template = (
+const template = (
   <div>
     <h1>{ appData.title }</h1>
     { appData.subtitle && <p>{appData.subtitle}</p> }
     <p>{ appData.options && appData.options.length ? 'Options available: ' + appData.options.join(', ') : 'No options found.' }</p>
   </div>
 );
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
