3995ea90be27583fb5ab1cf315f2bbeb0b6e952e
Archit Chandra <architchandra@gmail.com>
2021-07-15 20:25:24 +0530

    Remove old non-component code for counter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -88,32 +88,3 @@ var Counter = function (_React$Component) {
 }(React.Component);
 
 ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
-
-// let count = 0;
-
-// const plusOne = () => {
-//   count++;
-//   reRender();
-// };
-// const minusOne = () => {
-//   count--;
-//   reRender();
-// };
-// const reset = () => {
-//   count = 0;
-//   reRender();
-// };
-
-// const reRender = () => {
-//   const templateTwo = (
-//     <div>
-//       <h1>Count: { count }</h1>
-//       <button onClick={ () => plusOne() }>+1</button>
-//       <button onClick={ () => minusOne() }>-1</button>
-//       <button onClick={ () => reset() }>Reset</button>
-//     </div>
-//   );
-
-//   ReactDOM.render(templateTwo, appRoot);
-// };
-// reRender();
