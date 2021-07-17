fb2cb6e9fcd00acc7b079106d56e28a74f2810d0
Archit Chandra <architchandra@gmail.com>
2021-07-17 23:09:54 +0530

    Set up Webpack - first pass

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// Webpack Config File
+
+
+const path = require('path');
+
+
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+};
