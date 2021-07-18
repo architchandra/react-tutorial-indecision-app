948cc29cc75d682876a1a3b3cf3e8cf04fbbce65
Archit Chandra <architchandra@gmail.com>
2021-07-18 12:29:09 +0530

    Explore changing default export name when importing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import isAlive, { isAdult, canDrink } from './person.js';
+import isExist, { isAdult, canDrink } from './person.js';
 
 
 console.log(isAdult(24));
 console.log(canDrink(24));
-console.log(isAlive(-2));
+console.log(isExist(-2));
 console.log('App.js is running!');
