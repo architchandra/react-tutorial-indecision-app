4350a30c9ca992f6649ccc89ecbdf21759723870
Archit Chandra <architchandra@gmail.com>
2021-07-18 21:43:41 +0530

    Use Transform Class Properties to remove constructor in components, place lifecycle methods after event handlers

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -3,16 +3,11 @@ import React from 'react';
 
 
 class AddOption extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-
-    this.state = {
-      error: undefined,
-    };
-  }
+  state = {
+    error: undefined,
+  };
   
-  handleAddOption(e) {
+  handleAddOption = (e) => {
     e.preventDefault();
     
     const option = e.target.elements.option.value.trim();
@@ -25,7 +20,7 @@ class AddOption extends React.Component {
     if(!error) {
       e.target.elements.option.value = '';
     }
-  }
+  };
   
   render() {
     return (
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -7,18 +7,40 @@ import Header from './Header';
 
 
 class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handleChooseOption = this.handleChooseOption.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteSingleOption = this.handleDeleteSingleOption.bind(this);
+  state = {
+    options: this.props.options,
+    selectedOption: '',
+  };
+  
+  handleDeleteOptions = () => {
+    this.setState(() => ({
+      options: [],
+    }));
+  };
 
-    this.state = {
-      options: props.options,
-      selectedOption: '',
-    };
-  }
+  handleDeleteSingleOption = (option) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((item) => item !== option),
+    }));
+  };
+
+  handleChooseOption = () => {
+    this.setState(() => ({
+      selectedOption: this.state.options[Math.floor(Math.random() * this.state.options.length)],
+    }));
+  };
+
+  handleAddOption = (option) => {
+    if(!option) {
+      return 'Enter a valid option.';
+    } else if(this.state.options.indexOf(option) > -1) {
+      return 'This option already exists.';
+    }
+    
+    this.setState((prevState) => ({
+      options: prevState.options.concat(option),
+    }));
+  };
 
   componentDidMount() {
     try {
@@ -39,36 +61,6 @@ class IndecisionApp extends React.Component {
       const optionsJson = JSON.stringify(this.state.options);
       localStorage.setItem('options', optionsJson);
     }
-  }
-  
-  handleDeleteOptions() {
-    this.setState(() => ({
-      options: [],
-    }));
-  }
-
-  handleDeleteSingleOption(option) {
-    this.setState((prevState) => ({
-      options: prevState.options.filter((item) => item !== option),
-    }));
-  }
-
-  handleChooseOption() {
-    this.setState(() => ({
-      selectedOption: this.state.options[Math.floor(Math.random() * this.state.options.length)],
-    }));
-  }
-
-  handleAddOption(option) {
-    if(!option) {
-      return 'Enter a valid option.';
-    } else if(this.state.options.indexOf(option) > -1) {
-      return 'This option already exists.';
-    }
-    
-    this.setState((prevState) => ({
-      options: prevState.options.concat(option),
-    }));
   }
   
   render() {
