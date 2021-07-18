87524b595a90706b2527e06a1c24104c2ff47436
Archit Chandra <architchandra@gmail.com>
2021-07-18 13:22:56 +0530

    Install React, React DOM and Validator

--- src/app.js
+++ src/app.js
@@ -1,7 +1,2 @@
-import isAlive, { isAdult, canDrink } from './person.js';
-
-
-console.log(isAdult(24));
-console.log(canDrink(24));
-console.log(isAlive(-2));
-console.log('App.js is running!');
+import React from 'react';
+import ReactDOM from 'react-dom';
