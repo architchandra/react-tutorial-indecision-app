2fb08c5409ad0ff0d1d2c86302b0b3201335780e
Archit Chandra <architchandra@gmail.com>
2021-07-19 12:43:09 +0530

    Allow modal to close using keyboard and by clicking outside modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,6 +6,7 @@ import Modal from 'react-modal';
 const OptionModal = (props) => (
   <Modal
     isOpen={ !!props.selectedOption }
+    onRequestClose={ props.handleClearSelectedOption }
     contentLabel="Selected Option"
   >
     <h3>Selected Option</h3>
