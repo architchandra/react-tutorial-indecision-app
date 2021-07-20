af3f16acb111782a30c5729461e25508826b48df
Archit Chandra <architchandra@gmail.com>
2021-07-20 11:52:55 +0530

    Enable SCSS usage

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,11 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
     }, {
-      test: /\.css$/,
+      test: /\.scss$/,
       use: [
         'style-loader',
         'css-loader',
+        'sass-loader',
       ],
     }],
   },
