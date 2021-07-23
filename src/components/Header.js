d5a5d37ba760018c3d40a7349738c87457af7b2f
Archit Chandra <architchandra@gmail.com>
2021-07-23 13:45:38 +0530

    Fix typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = (props) => (
   <header className="header">
     <div className="container">
-      <h1 class="header__title">{ props.title }</h1>
+      <h1 className="header__title">{ props.title }</h1>
       { props.subtitle && <h2 className="header__subtitle">{ props.subtitle }</h2> }
     </div>
   </header>
